21ce471f11a27c9b7d8ff24ac48959e68aca8b1b
NickCarreiro <135732586+NickCarreiro@users.noreply.github.com>
2023-11-16 17:19:44 -0500

    Update remove.php

--- remove.php
+++ remove.php
@@ -3,6 +3,6 @@ include 'connect.php';
 if (isset($_GET['remove'])) {
   echo 'remove was selected';
 }
-$sql = "DELETE FROM <database>  WHERE <col1> = '<ProductID>'";
+$sql = "DELETE FROM <database>  WHERE <col1> = $ID";
 mysqli_query($connection, $sql); //References back to code in connect.php
 <?>

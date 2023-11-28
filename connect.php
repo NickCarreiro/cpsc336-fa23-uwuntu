8027ee8cc42b5d63b0cfcfea7345377afb6d34ac
Cas (Jessi) <94129948+cronchy-cannoli@users.noreply.github.com>
2023-11-28 17:55:25 -0500

    Update connect.php

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 $servername  = “uwuntu.cka8ngtdpp3k.us-east-1.rds.amazonaws.com”; //Provided from RDS instance
 $username     = “username”;
 $password     = “password”;
-$dbname       = “<RDS database name>”; //Should be CPSC336FinProj
+$dbname       = “uwuntu”; //Should be CPSC336FinProj
 
 //create connection
 $connection = mysqli_connect($servername, $username, $password, $dbname);

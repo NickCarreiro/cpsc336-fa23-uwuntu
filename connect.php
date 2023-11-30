d2ae51808948e794547306ebafe816c40cc810a3
NickCarreiro <135732586+NickCarreiro@users.noreply.github.com>
2023-11-30 17:23:25 -0500

    Update connect.php
    comments out for prod

--- connect.php
+++ connect.php
@@ -1,8 +1,13 @@
 <?php>
-$servername  = “uwuntu.cka8ngtdpp3k.us-east-1.rds.amazonaws.com”; //Provided from RDS instance
-$username     = “username”;
-$password     = “password”;
-$dbname       = “uwuntu”; //Should be CPSC336FinProj
+//$servername  = “uwuntu.cka8ngtdpp3k.us-east-1.rds.amazonaws.com”; Provided from RDS instance
+//$username     = “username”;
+//$password     = “password”;
+//$dbname       = “uwuntu”; //Should be CPSC336FinProj
+
+$servername = "127.0.0.1";
+$username = "student";
+$password = "student";
+$dbname = "inventory";
 
 //create connection
 $connection = mysqli_connect($servername, $username, $password, $dbname);

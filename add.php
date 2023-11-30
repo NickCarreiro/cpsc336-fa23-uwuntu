81d4b68d4c149a14499b6ad5c2633394e81597be
NickCarreiro <135732586+NickCarreiro@users.noreply.github.com>
2023-11-30 17:34:49 -0500

    Update add.php

--- add.php
+++ add.php
@@ -1,8 +1,8 @@
-<?php>
+<?php
 include 'connect.php';
 if (isset($_GET['add'])) {
   echo 'key1 is set';
 }
 $sql = "INSERT INTO <database> (<col1>, <col2>, <col3>) VALUES (<col1 data pulled from html>, <col2 data pulled from html>,  <col3 data pulled from html>)";
 mysqli_query($connection, $sql); //References back to code in connect.php
-<?> 
+?> 

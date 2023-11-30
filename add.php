b58054d1621b99b5478a82a641c99313ed4d0e91
NickCarreiro <135732586+NickCarreiro@users.noreply.github.com>
2023-11-30 18:17:58 -0500

    Update add.php

--- add.php
+++ add.php
@@ -1,8 +1,35 @@
 <?php
 include 'connect.php';
-if (isset($_GET['add'])) {
-  echo 'key1 is set';
+
+// Check if the URL parameters are set
+if (isset($_GET['ID'], $_GET['ItemName'], $_GET['Quantity'])) {
+    // Sanitize inputs
+    $ID = mysqli_real_escape_string($connection, $_GET['ID']);
+    $ItemName = mysqli_real_escape_string($connection, $_GET['ItemName']);
+    $Quantity = mysqli_real_escape_string($connection, $_GET['Quantity']);
+
+    // Prepare SQL statement
+    $stmt = mysqli_prepare($connection, "INSERT INTO stock (id, item_name, quantity) VALUES (?, ?, ?)");
+    if ($stmt === false) {
+        die('Prepare failed: ' . htmlspecialchars(mysqli_error($connection)));
+    }
+
+    // Bind parameters to the prepared statement
+    mysqli_stmt_bind_param($stmt, 'isi', $ID, $ItemName, $Quantity);
+
+    // Execute the statement
+    if (mysqli_stmt_execute($stmt)) {
+        echo "Record added successfully.";
+    } else {
+        echo "Error adding record: " . htmlspecialchars(mysqli_stmt_error($stmt));
+    }
+
+    // Close the statement
+    mysqli_stmt_close($stmt);
+} else {
+    echo "Required data missing.";
 }
-$sql = "INSERT INTO stock (id, item_name, quantity) VALUES ($ID, $ItemName, $Quantity)";
-mysqli_query($connection, $sql); //References back to code in connect.php
-?> 
+
+// Close the connection
+mysqli_close($connection);
+?>

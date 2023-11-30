328a17e6dcef7214417d76e014e4723899d9c50b
NickCarreiro <135732586+NickCarreiro@users.noreply.github.com>
2023-11-30 17:43:17 -0500

    Update add.php

--- add.php
+++ add.php
@@ -3,6 +3,6 @@ include 'connect.php';
 if (isset($_GET['add'])) {
   echo 'key1 is set';
 }
-$sql = "INSERT INTO <database> (<col1>, <col2>, <col3>) VALUES (<col1 data pulled from html>, <col2 data pulled from html>,  <col3 data pulled from html>)";
+$sql = "INSERT INTO stock (id, item_name, quantity) VALUES ($ID, $ItemName, $Quantity)";
 mysqli_query($connection, $sql); //References back to code in connect.php
 ?> 

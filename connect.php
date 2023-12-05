f177a74300e1cb978a367b9ee7f3e1447d3ed9dc
root <root@ip-172-31-19-8.ec2.internal>
2023-12-05 02:46:03 +0000

    updated connect.php

--- connect.php
+++ connect.php
@@ -1,13 +1,13 @@
 <?php
-//$servername  = “uwuntu.cka8ngtdpp3k.us-east-1.rds.amazonaws.com”; Provided from RDS instance
-//$username     = “username”;
-//$password     = “password”;
-//$dbname       = “uwuntu”; //Should be CPSC336FinProj
+$servername  = 'uwuntu-db.cka8ngtdpp3k.us-east-1.rds.amazonaws.com';
+$username     = 'username';
+$password     = 'password';
+$dbname       = 'inventory';
 
-$servername = "127.0.0.1";
-$username = "root";
-$password = "student";
-$dbname = "inventory";
+//$servername = "127.0.0.1";
+//$username = "root";
+//$password = "student";
+//$dbname = "inventory";
 
 //create connection
 $connection = mysqli_connect($servername, $username, $password, $dbname);
